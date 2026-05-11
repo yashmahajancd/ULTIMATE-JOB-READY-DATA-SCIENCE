653c34b5fec435a9caae3d203eed6143db3fd64e
Yash Mahajan <mahajanyash2185@gmail.com>
2026-05-11 23:55:11 +0530

    plotting charts using matplotlib

---
+++ 08-matplotlib/matplotlib_demo.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# plt.plot([1, 2, 3], [4, 5, 6])
+# plt.show()
+
+years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010] 
+runs =  [500, 700, 1100, 1500, 1800, 1200, 1700, 1300, 900, 1500]
+
+plt.plot(years, runs)
+plt.xlabel("Years")
+plt.ylabel("Runs")
+plt.title("Sachin Tendulkar's Yearly Runs")
+plt.show()
