a0716308dcd67b92493d9adcb723eca53b39b251
Yash Mahajan <mahajanyash2185@gmail.com>
2026-05-18 21:20:50 +0530

    creating a databases

--- 10-sql-for-data-science/02_creating_databases.sql
+++ 10-sql-for-data-science/02_creating_databases.sql
@@ -18,8 +18,58 @@ DROP DATABASE student;
 -- Creating a Table in MySQL
 CREATE TABLE students (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL DEFAULT 'No Name',
+    name VARCHAR(100),
     age INT,
     email VARCHAR(100) UNIQUE,
-    admission_date DATE
+    admission_data DATE
 );
+
+CREATE TABLE std (
+    id INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+
+-- View All Tables
+SHOW TABLES;
+
+
+-- View Table Structure
+DESCRIBE students;
+
+
+-- Viewing Table Data
+SELECT * FROM students;
+
+
+
+------ Modifying a Table in MySQL ------
+
+-- Renaming a Table
+RENAME TABLE students TO vtp_students;
+
+
+-- Dropping a Table
+DROP TABLE std;
+
+
+-- Renaming a Column
+ALTER TABLE vtp_students RENAME COLUMN admission_data TO admission_date;
+
+
+-- Adding a Column
+ALTER TABLE vtp_students ADD COLUMN is_passed BOOL DEFAULT True;
+ALTER TABLE students ADD COLUMN gender VARCHAR(10);
+
+
+-- Dropping a Column
+ALTER TABLE table_name DROP COLUMN gender;
+
+
+-- Modifying a Column
+ALTER TABLE vtp_students MODIFY COLUMN name VARCHAR(50) NOT NULL DEFAULT 'No Name';
+
+
+-- Changing the Order of Columns
+ALTER TABLE vtp_students MODIFY COLUMN is_passed BOOL DEFAULT True AFTER 
+name;
