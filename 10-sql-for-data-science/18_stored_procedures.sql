b2edeef06d9417a073e1a7fbaac18af125588cce
Yash Mahajan <mahajanyash2185@gmail.com>
2026-05-24 01:47:51 +0530

    stored procedures in mysql

--- 10-sql-for-data-science/18_stored_procedures.sql
+++ 10-sql-for-data-science/18_stored_procedures.sql
@@ -29,3 +29,45 @@ CALL list_employees();
 ------ Stored Procedure with Parameters
 
 ---- Example: Get details of an employee by ID
+DELIMITER //
+
+CREATE PROCEDURE get_employee_by_id(IN p_emp_id INT)
+
+BEGIN
+	SELECT * FROM employee WHERE emp_id = p_emp_id;
+END //
+
+DELIMITER ;
+
+
+---- Call it
+CALL get_employee_by_id(3);
+
+-----------------------------------------------------------------------
+
+------ Dropping a Stored Procedure
+
+---- To delete a stored procedure
+DROP PROCEDURE IF EXISTS get_employee_by_id;
+
+-----------------------------------------------------------------------
+-----------------------------------------------------------------------
+
+---- Example: Get details of an employee by Department
+DELIMITER //
+
+CREATE PROCEDURE get_emp_by_dept(IN dept VARCHAR(20))
+
+BEGIN
+	SELECT * FROM employee WHERE department = dept;
+END //
+
+DELIMITER ;
+
+
+---- Call it
+CALL get_emp_by_dept('IT');
+
+
+---- Delete it
+DROP PROCEDURE IF EXISTS get_emp_by_dept;
