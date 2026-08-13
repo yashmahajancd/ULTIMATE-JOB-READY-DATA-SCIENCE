cef9f588bdcea6b908ba441fddc5615496a65002
Yash Mahajan <mahajanyash2185@gmail.com>
2026-08-13 19:13:09 +0530

    data preprocessing - final pipeline

---
+++ 15-practical-ml-using-scikit-learn/17-columntransformer-and-consolidating-the-pipeline/project-gurgaon/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+# 1. Load the dataset
+housing = pd.read_csv("housing.csv")
