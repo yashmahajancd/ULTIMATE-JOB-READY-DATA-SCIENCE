029abf0a75d77232c680872e74b86e737326fd37
Yash Mahajan <mahajanyash2185@gmail.com>
2026-08-14 17:41:07 +0530

    data preprocessing - final pipeline

--- 15-practical-ml-using-scikit-learn/17-columntransformer-and-consolidating-the-pipeline/project-gurgaon/main.py
+++ 15-practical-ml-using-scikit-learn/17-columntransformer-and-consolidating-the-pipeline/project-gurgaon/main.py
@@ -22,9 +22,32 @@ for train_index, test_index in split.split(housing, housing["income_cat"]):
 housing = strat_train_set.copy()
 
 # 3. Seperate features and lables
-housing_lables = housing["median_house_value"].copy()
+housing_labels = housing["median_house_value"].copy()
 housing = housing.drop("median_house_value", axis=1)
 
-# 4. Seperate numerical and categorical columns
+# 4. List the numerical and categorical columns
 num_attribs = housing.drop("ocean_proximity", axis=1).columns.to_list()   # ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income']
-cat_attribs = ["ocean_proximity"]
+cat_attribs = ["ocean_proximity"]   # ['ocean_proximity']
+
+# 5. Lets make the pipeline
+
+# For numerical columns
+num_pipeline = Pipeline([
+    ("imputer", SimpleImputer(strategy="median")),
+    ("scaler", StandardScaler()),
+])
+
+# For categorical columns
+cat_pipeline = Pipeline([
+    ("onehot", OneHotEncoder(handle_unknown="ignore"))
+])
+
+# Construct the full pipeline
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", cat_pipeline, cat_attribs),
+])
+
+# 6. Transform the data
+housing_prepared = full_pipeline.fit_transform(housing)
+print(housing_prepared.shape)
