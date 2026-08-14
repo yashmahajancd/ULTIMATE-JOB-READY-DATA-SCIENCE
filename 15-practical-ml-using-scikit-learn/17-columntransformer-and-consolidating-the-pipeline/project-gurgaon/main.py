b1b8ecec4b61c71c21bb409b3a8bcd709ff35f8f
Yash Mahajan <mahajanyash2185@gmail.com>
2026-08-14 16:29:01 +0530

    data preprocessing - final pipeline

--- 15-practical-ml-using-scikit-learn/17-columntransformer-and-consolidating-the-pipeline/project-gurgaon/main.py
+++ 15-practical-ml-using-scikit-learn/17-columntransformer-and-consolidating-the-pipeline/project-gurgaon/main.py
@@ -8,3 +8,15 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 # 1. Load the dataset
 housing = pd.read_csv("housing.csv")
+
+# Create a stratified test set
+housing["income_cat"] = pd.cut(housing["median_income"], bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf], labels=[1, 2, 3, 4, 5])
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index].drop("income_cat", axis=1)
+    strat_test_set = housing.loc[test_index].drop("income_cat", axis=1)
+
+# We will work on the copy of training data
+housing = strat_train_set.copy()
