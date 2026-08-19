8ba3140f2cb823547092493055f161cb257ddd63
Yash Mahajan <mahajanyash2185@gmail.com>
2026-08-19 23:26:17 +0530

    training and evaluating ml models

---
+++ 15-practical-ml-using-scikit-learn/18-training-ml-algorithms-on-preprocessed-data/project-gurgaon/main.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+
+# 1. Load the dataset
+housing = pd.read_csv("housing.csv")
+
+# Create a stratified test set
+housing["income_cat"] = pd.cut(housing["median_income"], bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf], labels=[1, 2, 3, 4, 5])
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index].drop("income_cat", axis=1)
+    strat_test_set = housing.loc[test_index].drop("income_cat", axis=1)
+
+# We will work on the copy of training data
+housing = strat_train_set.copy()
+
+# 3. Seperate features and lables
+housing_labels = housing["median_house_value"].copy()
+housing = housing.drop("median_house_value", axis=1)
+
+# 4. List the numerical and categorical columns
+num_attribs = housing.drop("ocean_proximity", axis=1).columns.to_list()   # ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income']
+cat_attribs = ["ocean_proximity"]   # ['ocean_proximity']
+
+# 5. Lets make the pipeline
+
+# For numerical columns
+num_pipeline = Pipeline([
+    ("imputer", SimpleImputer(strategy="median")),
+    ("scaler", StandardScaler()),
+])
+
+# For categorical columns
+cat_pipeline = Pipeline([
+    ("onehot", OneHotEncoder(handle_unknown="ignore"))
+])
+
+# Construct the full pipeline
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", cat_pipeline, cat_attribs),
+])
+
+# 6. Transform the data
+housing_prepared = full_pipeline.fit_transform(housing)
+print(housing_prepared.shape)
+
+# 7. Train the model
+
+# Linear Regression Model
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared, housing_labels)
+lin_preds = lin_reg.predict(housing_prepared)
+lin_rmse = root_mean_squared_error(housing_labels, lin_preds)
+print(lin_rmse)
